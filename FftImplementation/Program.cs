5c137154120ff52ff567efc33811fcdcce46a8ac
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2023-03-25 14:51:22 +0100

    Fft implemented

--- FftImplementation/Program.cs
+++ FftImplementation/Program.cs
@@ -24,12 +24,24 @@ namespace FftImplementation
     [STAThread]
     private static void Main(string[] args)
     {
-      var n = 128;
+      var n = 256;
       var coefficients = new Complex[n];
       coefficients[10] = new Complex(1.0, 0.0);
+      coefficients[15] = new Complex(0.5, 0.0);
 
       int i = 0;
       DrawGraph(coefficients.Select(c => new DataPoint(i++, c.Real)));
+
+      var fft = new Fft(n);
+      var wave = fft.Transform(coefficients);
+
+      i = 0;
+      DrawGraph(wave.Select(c => new DataPoint(i++, c.Real)));
+
+      var wave2 = fft.Transform(wave);
+
+      i = 0;
+      DrawGraph(wave2.Reverse().Select(c => new DataPoint(i++, c.Real)));
     }
 
     #endregion Methods
